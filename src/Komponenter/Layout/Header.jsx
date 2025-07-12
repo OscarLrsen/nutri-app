df103cada7ea5d0a4d21a4664a59c41802fbd9de
Oscar <oscar.larsen@edu.newton.se>
2025-07-12 16:16:28 +0200

    La til nya filer och börjat med headern för home

--- src/Komponenter/Layout/Header.jsx
+++ src/Komponenter/Layout/Header.jsx
@@ -1,40 +1,28 @@
 import React from "react";
-import "./HeaderFooter.css";
-import { useCart } from "../../Kontext/CartContext";
-import CartButton from "../Cart/CartButton";
 import { useNavigate } from "react-router-dom";
+import "./Headerfooter.css";
 
 export default function Header() {
-  const { cartItems } = useCart();
-  const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
   const navigate = useNavigate();
 
   return (
-    <header>
-      {/* Klickbar logotyp */}
-      <div className="topLogo" onClick={() => navigate("/")}>
-        NUTRI
+    <header className="header">
+      <div className="header">
+        <div className="header-left" onClick={() => console.log("öppna meny")}>
+          <span className="material-icons">menu</span>
+        </div>
+
+        <div className="header-center" onClick={() => navigate("/")}>
+          Nutri
+        </div>
+
+        <div className="header-right">
+          <span className="login-text" onClick={() => navigate("/login")}>
+            Logga in
+          </span>
+        </div>
       </div>
 
-      <div className="nav-buttons">
-        <nav>
-          <span className="nav-link" onClick={() => navigate("/")}>Home</span>
-          <span className="nav-link"onClick={() => window.open("https://www.newton.se/", "_blank")}>About Us</span>
-          <span className="nav-link"onClick={() => window.open("https://www.newton.se/", "_blank")}>Contact Us</span>
-          <span className="btn-nav-login"onClick={() => navigate("/Login")}>Login</span>
-        </nav>
-
-      <button 
-        className="btn btn-yellow" 
-        onClick={() => window.open("https://www.google.com/maps/place/Norra+Bulltoftavägen+65C,+212+43+Malmö", "_blank")}
-      >
-        Find Us
-      </button>
-
-
-
-        <CartButton totalItems={totalQuantity} />
-      </div>
     </header>
   );
 }
