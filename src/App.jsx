ab64f7556c2650dd07a141d84f54e1c7613e4f71
Oscar <oscar.larsen@edu.newton.se>
2025-07-19 09:49:59 +0200

    Headern är ändrad

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Komponenter/Home/Home";
 import Menu from "./Komponenter/Menu/Menu";
-import Cart from "./Komponenter/Cart/Cart"; 
-import Login from "./Komponenter/Login/Login"; 
+import Cart from "./Komponenter/Cart/Cart";
+import Login from "./Komponenter/Login/Login";
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} /> 
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/Komponenter/Layout/Header.jsx
+++ src/Komponenter/Layout/Header.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Headerfooter.css";
 
 export default function Header() {
 
+
+const [menuOpen, setMenuOpen] = useState(false);
+
+const toggleMenu = () => {
+  setMenuOpen(prev => !prev);
+};
+
   return (
     <header className="nutri-header">
-      <div className="menu">
-        <button className="menu-btn">☰ Meny</button>
+      {/* Menyn som skjuts in */}
+      <div className={`off-screen-menu ${menuOpen ? "active" : ""}`}>
+        <ul>
+          <li onClick={toggleMenu}>Home</li>
+          <li onClick={toggleMenu}>About</li>
+          <li onClick={toggleMenu}>Contact</li>
+        </ul>
       </div>
 
+      {/* Hamburgerknapp */}
+      <nav className="menu">
+        <div
+          className={`ham-menu ${menuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+
+      {/* Logotyp */}
       <div className="logo">
         <a href="/">
           <img src="src\Bilder\NUTRIVIT.jpeg" alt="NUTRI" />
         </a>
       </div>
 
+      {/* Login-länk */}
       <div className="login">
         <a href="/login">Logga in</a>
       </div>
